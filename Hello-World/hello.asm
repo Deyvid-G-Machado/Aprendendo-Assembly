b21bee9dd2d5649339aa3017a6c60494b818c2d1
Deyvid-G-Machado <deyvid.zimba@hotmail.com>
2023-08-12 01:56:51 -0300

    Adicionado Hello World

---
+++ Hello-World/hello.asm
@@ -0,0 +1,26 @@
+; Compilar - Transformar o programa para linguagem máquina
+;   nasm -f elf64 hello.asm
+; Linkeditar - Transformar o programa em linguagem de máquina para um executável
+;   ld -s -o hello hello.o
+
+section .data
+    msg db 'Hello World!', 0xA
+    msg1 db 'Essa é uma mensagem aleatoria!', 0xA  
+    tam equ $- msg
+
+
+section .text
+
+global _start
+
+_start:
+    mov EAX, 0x4    ; Fazer uma saida
+    mov EBX, 0x1    ; Saida padrao
+    mov ECX, msg    ; Mensagem
+    mov EDX, tam    ; Tamanho
+    int 0x80
+
+saida:
+    mov EAX, 0X1 ; SO estou terminando o programa
+    mov EBX, 0X0 ; SO o valor de retorno é 0
+    int 0X80
